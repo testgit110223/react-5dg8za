f3a053b297efdd47e9b7f54b396318372f1a2bd2
testgit110223 <125124832+testgit110223@users.noreply.github.com>
2023-02-15 21:04:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-5dg8za
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-5dg8za)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>What a wonderful worldZZ :)</p>
+    </div>
+  );
+}
